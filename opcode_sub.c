1eec6836269ee8f978d8995487db8d648fe9ea1b
Shighi <daisyshighi@gmail.com>
2024-09-26 09:04:48 -0700

    sub2

--- opcode_sub.c
+++ opcode_sub.c
@@ -1,20 +1,19 @@
 #include "monty.h"
 
 /**
- * opcode_sub - Subtracts the top element from the second top element.
- * @stack: Double pointer to the stack (head).
- * @line_number: Line number of the bytecode file (counter).
+ * opcode_sub - Subtracts the top element from the second top element
+ * @stack: Double pointer to the stack (head)
+ * @line_number: Line number of the bytecode file
+ *
+ * Description: Performs subtraction operation on the top two stack elements.
+ * If the stack is too short, it prints an error message and exits.
  */
 void opcode_sub(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
-	int diff, nodes;
+	int diff;
 
-	temp = *stack;
-	for (nodes = 0; temp != NULL; nodes++)
-		temp = temp->next;
-
-	if (nodes < 2)
+	if (!*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		fclose(bus.file);
@@ -27,5 +26,6 @@ void opcode_sub(stack_t **stack, unsigned int line_number)
 	diff = temp->next->n - temp->n;
 	temp->next->n = diff;
 	*stack = temp->next;
+	(*stack)->prev = NULL;
 	free(temp);
 }
